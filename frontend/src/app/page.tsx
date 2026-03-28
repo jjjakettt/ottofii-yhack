cc324d4606de83a58ab3df3611858a409fbd8f0c
Emmanuel Kasuti Makau <emmanuel.k.makau.jr.26@dartmouth.edu>
2026-03-28 16:39:43 -0400

    feat: add ConnectPage component and update Tailwind configuration

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
 import Navbar from "./components/Navbar";
+import ConnectPage from "./components/ConnectPage";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex flex-col items-center justify-center flex-1 bg-gray-50">
-      </main>
+      <ConnectPage />
     </div>
   );
 }
